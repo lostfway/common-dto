ecb61eb4446ec4891b0a3af582b298011dfa84f2
lostway <lostyway@github.com>
2025-11-03 22:29:26 +0300

    4

---
+++ src/main/java/org/lostway/kafka/OrderKafkaEvent.java
@@ -0,0 +1,30 @@
+package org.lostway.kafka;
+
+import org.lostway.enums.Currency;
+import org.lostway.enums.OrderStatus;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+public record OrderKafkaEvent(
+    UUID orderId,
+    UUID userId,
+    UUID sellerId,
+    UUID paymentId,
+    UUID shipmentId,
+    OrderStatus status,
+    BigDecimal totalAmount,
+    Currency currency,
+    List<OrderItemKafkaEvent> items,
+    Instant slaDeadline,
+    Instant createdAt,
+    Instant updatedAt
+) {
+    public record OrderItemKafkaEvent(
+        UUID sku,
+        BigDecimal price
+    ) {
+    }
+}
